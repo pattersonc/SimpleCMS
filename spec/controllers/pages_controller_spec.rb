416106d5de9a5030bb3a2af5800ae5ec54f23377
chris <chris@chris-VirtualBox.(none)>
2011-12-05 15:50:37 -0500

    added some PageController specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
 
 describe PagesController do
-  describe "GET 'home'" do
+#  render_views
+
+  before(:each) do
+    page = double(:Page) do |p|
+      p.name = "example name",
+      p.title = "Example Title",
+      p.slug = "index",
+      p.content = "<div>hello world.</div>"
+    end
+
+    Page.stub(:content_page).and_return(page)
+  end
+
+  describe "GET 'content' without :id param" do
     it "should be successful" do
-      get 'home'
+      get 'content'
       response.should be_success
     end
   end
+
+  describe "GET 'content' with :id param" do
+    it "should be successful" do
+      get 'content', :id => 'example'
+      response.should be_success
+    end
+
+    it "should call content_page with :id param" do
+      Page.should_receive(:content_page).with('example')
+      get 'content', :id => 'example'
+    end
+  end
+
 end
