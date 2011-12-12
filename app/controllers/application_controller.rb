5933219dfe8cff506f3d6baed512fd958b2195f5
Chris Patterson <christopher.m.patterson@gmail.com>
2011-12-11 20:54:37 -0500

    additional changes for renaming widgets to posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   helper_method :page_links
-  helper_method :widgets
+  helper_method :posts
   
   def page_links
     Page.where(:active => true).order(:display_order)
