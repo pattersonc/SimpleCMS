6eeb385e7e994903225bfa75b0812ddb2fd755ed
Chris Patterson <christopher.m.patterson@gmail.com>
2011-12-26 17:09:23 -0500

    style updates for IE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,27 @@
 module ApplicationHelper
   
   def active_page?(slug)
-    if params[:slug].nil? and slug == 'index'
+    if params[:slug].nil? and params[:id].nil? and slug == 'index'
       return true
     end
     
     slug == params[:slug]
   end
+  
+  def active_message_page?(id)
+    id == params[:id]    
+  end
+  
+  def active_page1?(type, id)
+    result = if type == :page
+        params[:slug].nil? and id == :index or params[:slug] == id
+      elsif type == :message
+        params[:id] === id
+      else
+        false
+      end
+  end
+  
   
   def site_title
     base_title = Rails.application.config.site_title
