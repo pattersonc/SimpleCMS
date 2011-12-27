a07c18bf4eae479737b7787bdafc5ada43a10006
Chris Patterson <christopher.m.patterson@gmail.com>
2011-12-27 11:29:38 -0500

    started spinner javascript. updated seed tempalte

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,9 @@
 module ApplicationHelper
   
-  def active_page?(slug)
-    if params[:slug].nil? and params[:id].nil? and slug == 'index'
-      return true
-    end
-    
-    slug == params[:slug]
-  end
-  
-  def active_message_page?(id)
-    id == params[:id]    
-  end
-  
-  def active_page1?(type, id)
+  def active_page?(type, id)
     result = if type == :page
-        params[:slug].nil? and id == :index or params[:slug] == id
+        (params[:id].nil? and params[:slug].nil? and id == 'index') or 
+          params[:slug] == id
       elsif type == :message
         params[:id] === id
       else
